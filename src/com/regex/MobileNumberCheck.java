3bfc066dfbd1f376a44d78c1243d9118da0cd9d4
Your Name <nanawarechaitali4196@gmail.com>
2023-03-02 16:10:30 +0530

    Added mobileumberChecker

--- src/com/regex/MobileNumberCheck.java
+++ src/com/regex/MobileNumberCheck.java
@@ -1,4 +1,30 @@
 package com.regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MobileNumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter minimum 8 characters password");
+        String password = sc.nextLine();
+        boolean isValid = Validate(password);
+
+    }
+
+    private static boolean Validate(String password) {
+        String passwordRegex = "^[a-zA-Z0-9]{8,}";
+        Pattern pattern = Pattern.compile(passwordRegex);
+        Matcher matcher = pattern.matcher(password);
+
+
+        if(matcher.matches()==false){
+            System.out.println("Does not follows");
+        }else{
+            System.out.println("Follows");
+        }
+        return true;
+
+    }
 }
